9a33bad4f28f4d38aebc810108e86d3989d16c22
dchen <dongmeic@uoregon.edu>
2018-06-04 23:41:51 -0700

    Add field

--- python/beetlegrids.py
+++ python/beetlegrids.py
@@ -18,98 +18,87 @@ start = timeit.default_timer()
 arcpy.env.workspace = "H:/beetle/data/vector/mpbdata/reprojected"
 outfolder = "H:/beetle/output/climate_space/presence/"
 
-try:
-    mask = "H:/beetle/data/vector/na10km_v2/reproject/na10km_mask_pts.shp"
-    dsc_mask = arcpy.Describe(mask)
-    arcpy.MakeFeatureLayer_management(mask,"mask")
-    dist = "10000 Meters"
-        
-    for year in range(1997,2001):
-        bcfc = "bc_mpb_points_" + str(year) + ".shp"
-        bcpoly = "bc_mpb_poly_" + str(year) + ".shp"
-        usfc = "us_mpb_" + str(year)+".shp"
-        arcpy.MakeFeatureLayer_management(bcfc,"bcfc_{0}".format(year))
+
+mask = "H:/beetle/data/vector/na10km_v2/reproject/na10km_mask_pts.shp"
+arcpy.MakeFeatureLayer_management(mask,"mask")
+dist = "10000 Meters"
+    
+for year in range(1997,2001):
+    bcfc = "bc_mpb_points_" + str(year) + ".shp"
+    bcpoly = "bc_mpb_poly_" + str(year) + ".shp"
+    usfc = "us_mpb_" + str(year)+".shp"
+    arcpy.MakeFeatureLayer_management(bcfc,"bcfc_{0}".format(year))
+    if year != 1999:
         arcpy.MakeFeatureLayer_management(bcpoly,"bcpoly_{0}".format(year))
-        arcpy.MakeFeatureLayer_management(usfc,"usfc_{0}".format(year))
-        fieldList = arcpy.ListFields("mask")
-        for field in fieldList:
-            if field == "prs_"+ str(year):
-                break
-            else:
-                arcpy.AddField_management("mask", "prs_"+ str(year), "SHORT", "", "", 10)
-        try:
-            arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "bcfc_{0}".format(year), dist, "NEW_SELECTION")
-            arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "bcpoly_{0}".format(year), dist, "ADD_TO_SELECTION")
-            arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "usfc_{0}".format(year), dist, "ADD_TO_SELECTION")
-            with arcpy.da.UpdateCursor("mask", ['prs_{0}'.format(year)]) as cursor:
-                for row in cursor:
-                    row[0] = 1
-                    cursor.updateRow(row)
-            del row
-            del cursor
-            arcpy.SelectLayerByLocation_management("mask", "SWITCH_SELECTION")
-            with arcpy.da.UpdateCursor("mask", ['prs_{0}'.format(year)]) as cursor:
-                for row in cursor:
-                    row[0] = 0
-                    cursor.updateRow(row)
-            del row
-            del cursor
-            arcpy.SelectLayerByAttribute_management("mask", "CLEAR_SELECTION")
-            print(arcpy.GetMessages(0))
-        except arcpy.ExecuteError:
-            print(arcpy.GetMessages(2))
+    arcpy.MakeFeatureLayer_management(usfc,"usfc_{0}".format(year))
+    fieldList = arcpy.ListFields("mask")
+    field_names = [f.name for f in fieldList]
+##    for field in fieldList:
+##        print("Field:", field.name, field.type)
+    if "prs_"+ str(year) in field_names:
+        continue
+    else:
+        arcpy.AddField_management("mask", "prs_"+ str(year), "SHORT", "", "", 10)
+    arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "bcfc_{0}".format(year), dist, "NEW_SELECTION")
+    if year != 1999:
+        arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "bcpoly_{0}".format(year), dist, "ADD_TO_SELECTION")
+    arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "usfc_{0}".format(year), dist, "ADD_TO_SELECTION")
+    with arcpy.da.UpdateCursor("mask", ['prs_{0}'.format(year)]) as cursor:
+        for row in cursor:
+            row[0] = 1
+            cursor.updateRow(row)
+    del row
+    del cursor
+    arcpy.SelectLayerByAttribute_management("mask", "SWITCH_SELECTION")
+    with arcpy.da.UpdateCursor("mask", ['prs_{0}'.format(year)]) as cursor:
+        for row in cursor:
+            row[0] = 0
+            cursor.updateRow(row)
+    del row
+    del cursor
+    arcpy.SelectLayerByAttribute_management("mask", "CLEAR_SELECTION")
+    print(arcpy.GetMessages(0))
+    print('{0} year update finished'.format(year))
+    
+for year in range(2001,2017):
+    abfc = "ab_mpb_points_" + str(year) + ".shp"
+    bcfc = "bc_mpb_points_" + str(year) + ".shp"
+    bcpoly = "bc_mpb_poly_" + str(year) + ".shp"
+    usfc = "us_mpb_" + str(year)+".shp"
+    arcpy.MakeFeatureLayer_management(abfc,"abfc_{0}".format(year))
+    arcpy.MakeFeatureLayer_management(bcfc,"bcfc_{0}".format(year))
+    arcpy.MakeFeatureLayer_management(bcpoly,"bcpoly_{0}".format(year))
+    arcpy.MakeFeatureLayer_management(usfc,"usfc_{0}".format(year))
+    fieldList = arcpy.ListFields("mask")
+    field_names = [f.name for f in fieldList]
+    if "prs_"+ str(year) in field_names:
+        continue
+    else:
+        arcpy.AddField_management("mask", "prs_"+ str(year), "SHORT", "", "", 10)
+    arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "abfc_{0}".format(year), dist, "NEW_SELECTION")
+    arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "bcfc_{0}".format(year), dist, "ADD_TO_SELECTION")
+    arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "bcpoly_{0}".format(year), dist, "ADD_TO_SELECTION")
+    arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "usfc_{0}".format(year), dist, "ADD_TO_SELECTION")
+    with arcpy.da.UpdateCursor("mask", ['prs_{0}'.format(year)]) as cursor:
+        for row in cursor:
+            row[0] = 1
+            cursor.updateRow(row)
+    del row
+    del cursor
+    arcpy.SelectLayerByAttribute_management("mask", "SWITCH_SELECTION")
+    with arcpy.da.UpdateCursor("mask", ['prs_{0}'.format(year)]) as cursor:
+        for row in cursor:
+            row[0] = 0
+            cursor.updateRow(row)
+    del row
+    del cursor
+    arcpy.SelectLayerByAttribute_management("mask", "CLEAR_SELECTION")
+    print(arcpy.GetMessages(0))
+    print('{0} year update finished'.format(year))
 
-        except Exception as ex:
-            print(ex.args[0])
-        print('{0} year update finished'.format(year))
-        
-    for year in range(2001,2017):
-        abfc = "ab_mpb_points_" + str(year) + ".shp"
-        bcfc = "bc_mpb_points_" + str(year) + ".shp"
-        bcpoly = "bc_mpb_poly_" + str(year) + ".shp"
-        usfc = "us_mpb_" + str(year)+".shp"
-        arcpy.MakeFeatureLayer_management(abfc,"abfc_{0}".format(year))
-        arcpy.MakeFeatureLayer_management(bcfc,"bcfc_{0}".format(year))
-        arcpy.MakeFeatureLayer_management(bcpoly,"bcpoly_{0}".format(year))
-        arcpy.MakeFeatureLayer_management(usfc,"usfc_{0}".format(year))
-        fieldList = arcpy.ListFields("mask")
-        for field in fieldList:
-            if field == "prs_"+ str(year):
-                break
-            else:
-                arcpy.AddField_management("mask", "prs_"+ str(year), "SHORT", "", "", 10)
-        try:
-            arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "abfc_{0}".format(year), dist, "NEW_SELECTION")
-            arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "bcfc_{0}".format(year), dist, "ADD_TO_SELECTION")
-            arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "bcpoly_{0}".format(year), dist, "ADD_TO_SELECTION")
-            arcpy.SelectLayerByLocation_management("mask", "WITHIN_A_DISTANCE", "usfc_{0}".format(year), dist, "ADD_TO_SELECTION")
-            with arcpy.da.UpdateCursor("mask", ['prs_{0}'.format(year)]) as cursor:
-                for row in cursor:
-                    row[0] = 1
-                    cursor.updateRow(row)
-            del row
-            del cursor
-            arcpy.SelectLayerByLocation_management("mask", "SWITCH_SELECTION")
-            with arcpy.da.UpdateCursor("mask", ['prs_{0}'.format(year)]) as cursor:
-                for row in cursor:
-                    row[0] = 0
-                    cursor.updateRow(row)
-            del row
-            del cursor
-            arcpy.SelectLayerByAttribute_management("mask", "CLEAR_SELECTION")
-            print(arcpy.GetMessages(0))
-        except arcpy.ExecuteError:
-            print(arcpy.GetMessages(2))
-
-        except Exception as ex:
-            print(ex.args[0])
-        print('{0} year update finished'.format(year))
-
-    arcpy.CopyFeatures_management("mask", outfolder+"na_beetle_presence.shp")
-    layer = "mask"
-    del layer
-except:
-   print arcpy.GetMessages()
+arcpy.CopyFeatures_management("mask", outfolder+"na_beetle_presence.shp")
+layer = "mask"
+del layer
 
 stop = timeit.default_timer()
 print stop - start
