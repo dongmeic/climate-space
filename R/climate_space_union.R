2c797c8c14fb9144d1a55f359e4d8b22d1ebab1e
dongmeic <dongmeijane1107@gmail.com>
2018-07-01 21:42:03 -0700

    btl_prs_stats

--- R/climate_space_union.R
+++ R/climate_space_union.R
@@ -2,11 +2,11 @@
 library(ncdf4)
 library(ggplot2)
 library(grid)
-library(animation)
-library(parallel)
-library(doParallel)
-library(foreach)
-registerDoParallel(cores=28)
+# library(animation)
+# library(parallel)
+# library(doParallel)
+# library(foreach)
+# registerDoParallel(cores=28)
 
 # functions
 # functions to return text in a boxplot
@@ -86,123 +86,101 @@ varnms.p <- c("Cumulative precipitation from Oct to Sep",
 			  "Precipitation from Jun to Aug in previous year",
 			  "Cumulative precipitation from Jun to Aug")
 
-varnms <- c("Minimum temperature in Oct",
-			  "Mean temperature from Sep to Nov",
-			  "Minimum winter temperature",
-			  "Minimum temperature in Jan",
-			  "Minimum temperature in Mar",
-			  "Mean minimum temperature from Nov to Mar",
-			  "Mean temperature from Aug to Jul",
-			  "Temperature variation from Aug to Jul",
-			  "Mean temperature from Oct to Sep",
-			  "Mean temperature from Mar to Aug",
-			  "Mean temperature from Jun to Aug",
-			  "Mean temperature in Aug",
-			  "Maximum temperature in Aug",
-			  "Cumulative precipitation from Oct to Sep",
-			  "Sum of precipitation from Jun to Aug",
-			  "Precipitation from Jun to Aug in previous year",
-			  "Cumulative precipitation from Jun to Aug",
-			  "Cumulative monthly Oct-Aug precipitation",
-			  "Mean precipitation from Aug to Jul",
-			  "Growing season precipitation",
-			  "Precipitation from Oct and Sep in previous year",
-			  "Sum of precipitation from Mar to Aug")
-
 cols <- c("grey70", "#1b9e77", "#d95f02")
 
-get.data <- function(var){
-  ncfile <- paste0("na10km_v2_",var, "_",years[1],".",years[nyr],".4d.nc")
-  ncin <- nc_open(paste0(ncpath, ncfile))
-  data <- ncvar_get(ncin,var)
-  fillvalue <- ncatt_get(ncin,var,"_FillValue")
-  data[data==fillvalue$value] <- NA
-  return(data)
-}
-
-get.dtcol <- function(var){
-  ndf <- data.frame(var=double(), prs=character(), yrs=numeric())
-  data <- get.data(var)
-  for (yr in 1:nyr){
-  	na <- data[,,1,yr]
-  	na <- na[!is.na(na)]
-  	vgt <- data[,,2,yr]
-  	vgt <- vgt[!is.na(vgt)]
-  	btl <- data[,,3,yr]
-  	btl <- btl[!is.na(btl)]
-  	valcol <- c(na, vgt, btl)
-  	prs <- c(rep("continent",length(na)),rep("hosts",length(vgt)),rep("mpb",length(btl)))
-  	yrs <- c(rep(years[yr],length(prs)))
-  	df <- data.frame(valcol,prs,yrs)
-  	ndf <- rbind(ndf, df)
-  }
-  colnames(ndf)[1] <- var
-  write.csv(ndf, paste0(outcsvpath, var, "_",years[1], "_",years[nyr], ".csv"), row.names = FALSE)
-}
-
-foreach(i = 1:length(vargrp))%dopar%{
-	get.dtcol(vargrp[i])
-}
-
-# reorganize the data table
-df <- read.csv(paste0(outcsvpath, vargrp[1], "_",years[1], "_",years[nyr], ".csv"))
-df1 <- data.frame(df[,vargrp[1]])
-colnames(df1) <- vargrp[1]
-df2 <- df[,2:3]
-for(i in 2:length(vargrp)){
-	df3 <- read.csv(paste0(outcsvpath, vargrp[i], "_",years[1], "_",years[nyr], ".csv"))
-	df4 <- data.frame(df3[,vargrp[i]])
-	colnames(df4) <- vargrp[i]
-	df1 <- cbind(df1, df4)
-	print(paste("adding the variable", vargrp[i]))
-}
-df5 <- cbind(df1, df2)
-write.csv(df5, "bioclimatic_variables_1996_2015.csv", row.names = FALSE)
-
-climate.space.paired <- function(i){
-  df <- df5[,c(vargrp.t[i], vargrp.p[i], "prs")]
-  colnames(df)[1:2] <- c("tmp", "pre")
-  plot1 <- qplot(tmp, pre, data=df, color=factor(prs), alpha=I(0.7), xlab = varnms.t[i], ylab = varnms.p[i], main = "MPB climate space")
-  plot1 <- plot1 + scale_colour_manual(name="Presencce", labels=c("Continent","Hosts","Beetles"), values = cols)+ labs(color="prs")
-  plot1 <- plot1 + theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"))
-  plot2 <- ggplot(df, aes(x=prs, y=tmp, fill=factor(prs)))+geom_boxplot()+scale_fill_manual(values = cols)+theme(axis.text.x=element_blank(),axis.ticks.x=element_blank())+labs(x="Presencce", y=varnms.t[i])+stat_summary(fun.data = max.n, geom = "text", fun.y = max)+
-    stat_summary(fun.data = min.n, geom = "text", fun.y = min)+stat_summary(fun.data = mean.n, geom = "text", fun.y = mean, col="white")+theme(legend.position="none")
-  plot3 <- ggplot(df, aes(x=prs, y=pre, fill=factor(prs)))+geom_boxplot()+scale_fill_manual(values = cols)+theme(axis.text.x=element_blank(),axis.ticks.x=element_blank())+labs(x="Presencce", y=varnms.p[i])+stat_summary(fun.data = max.n, geom = "text", fun.y = max)+
-    stat_summary(fun.data = min.n, geom = "text", fun.y = min)+stat_summary(fun.data = mean.n, geom = "text", fun.y = mean, col="white")+theme(legend.position="none")
-  
-  png(paste0(out,"cs_",vargrp.t[i],"_",vargrp.p[i],".png"), width=12, height=6, units="in", res=300)
-  grid.newpage()
-  par(mar=c(2,2,4,2))
-  pushViewport(viewport(layout = grid.layout(1, 4))) # 1 rows, 4 columns
-  print(plot1, vp = vplayout(1, 1:2))  # the big plot covers rows 1 and cols 1:2
-  print(plot2, vp = vplayout(1, 3))
-  print(plot3, vp = vplayout(1, 4))
-  dev.off()
-}
+# get.data <- function(var){
+#   ncfile <- paste0("na10km_v2_",var, "_",years[1],".",years[nyr],".4d.nc")
+#   ncin <- nc_open(paste0(ncpath, ncfile))
+#   data <- ncvar_get(ncin,var)
+#   fillvalue <- ncatt_get(ncin,var,"_FillValue")
+#   data[data==fillvalue$value] <- NA
+#   return(data)
+# }
+# 
+# get.dtcol <- function(var){
+#   ndf <- data.frame(var=double(), prs=character(), yrs=numeric())
+#   data <- get.data(var)
+#   for (yr in 1:nyr){
+#     na <- data[,,1,yr]
+#     na <- na[!is.na(na)]
+#     vgt <- data[,,2,yr]
+#     vgt <- vgt[!is.na(vgt)]
+#     btl <- data[,,3,yr]
+#     btl <- btl[!is.na(btl)]
+#     valcol <- c(na, vgt, btl)
+#     prs <- c(rep("continent",length(na)),rep("hosts",length(vgt)),rep("mpb",length(btl)))
+#     yrs <- c(rep(years[yr],length(prs)))
+#     df <- data.frame(valcol,prs,yrs)
+#     ndf <- rbind(ndf, df)
+#   }
+#   colnames(ndf)[1] <- var
+#   write.csv(ndf, paste0(outcsvpath, var, "_",years[1], "_",years[nyr], ".csv"), row.names = FALSE)
+# }
+# 
+# foreach(i = 1:length(vargrp))%dopar%{
+#   get.dtcol(vargrp[i])
+# }
+# 
+# # reorganize the data table
+# df <- read.csv(paste0(outcsvpath, vargrp[1], "_",years[1], "_",years[nyr], ".csv"))
+# df1 <- data.frame(df[,vargrp[1]])
+# colnames(df1) <- vargrp[1]
+# df2 <- df[,2:3]
+# for(i in 2:length(vargrp)){
+#   df3 <- read.csv(paste0(outcsvpath, vargrp[i], "_",years[1], "_",years[nyr], ".csv"))
+#   df4 <- data.frame(df3[,vargrp[i]])
+#   colnames(df4) <- vargrp[i]
+#   df1 <- cbind(df1, df4)
+#   print(paste("adding the variable", vargrp[i]))
+# }
+# df5 <- cbind(df1, df2)
+# write.csv(df5, "bioclimatic_variables_1996_2015.csv", row.names = FALSE)
+# 
+# climate.space.paired <- function(i){
+#   df <- df5[,c(vargrp.t[i], vargrp.p[i], "prs")]
+#   colnames(df)[1:2] <- c("tmp", "pre")
+#   plot1 <- qplot(tmp, pre, data=df, color=factor(prs), alpha=I(0.7), xlab = varnms.t[i], ylab = varnms.p[i], main = "MPB climate space")
+#   plot1 <- plot1 + scale_colour_manual(name="Presencce", labels=c("Continent","Hosts","Beetles"), values = cols)+ labs(color="prs")
+#   plot1 <- plot1 + theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"))
+#   plot2 <- ggplot(df, aes(x=prs, y=tmp, fill=factor(prs)))+geom_boxplot()+scale_fill_manual(values = cols)+theme(axis.text.x=element_blank(),axis.ticks.x=element_blank())+labs(x="Presencce", y=varnms.t[i])+stat_summary(fun.data = max.n, geom = "text", fun.y = max)+
+#     stat_summary(fun.data = min.n, geom = "text", fun.y = min)+stat_summary(fun.data = mean.n, geom = "text", fun.y = mean, col="white")+theme(legend.position="none")
+#   plot3 <- ggplot(df, aes(x=prs, y=pre, fill=factor(prs)))+geom_boxplot()+scale_fill_manual(values = cols)+theme(axis.text.x=element_blank(),axis.ticks.x=element_blank())+labs(x="Presencce", y=varnms.p[i])+stat_summary(fun.data = max.n, geom = "text", fun.y = max)+
+#     stat_summary(fun.data = min.n, geom = "text", fun.y = min)+stat_summary(fun.data = mean.n, geom = "text", fun.y = mean, col="white")+theme(legend.position="none")
+#   
+#   png(paste0(out,"cs_",vargrp.t[i],"_",vargrp.p[i],".png"), width=12, height=6, units="in", res=300)
+#   grid.newpage()
+#   par(mar=c(2,2,4,2))
+#   pushViewport(viewport(layout = grid.layout(1, 4))) # 1 rows, 4 columns
+#   print(plot1, vp = vplayout(1, 1:2))  # the big plot covers rows 1 and cols 1:2
+#   print(plot2, vp = vplayout(1, 3))
+#   print(plot3, vp = vplayout(1, 4))
+#   dev.off()
+# }
 
 # foreach(i = 1:length(vargrp.t))%dopar%{
 #   climate.space.paired(i)
 #   print(paste("The union climate space with variables", vargrp.t[i], "and", vargrp.p[i], "is done!"))
 # }
 
-n1 <- rep(c(1,2,3),5); n2 <- c(rep(1,3),rep(2,3),rep(3,3),rep(4,3),rep(5,3))
 climate.space <- function(i){
-  df <- df5[,c(vargrp.t[i], vargrp.p[i], "prs")]
-  colnames(df)[1:2] <- c("tmp", "pre")
-  p <- qplot(tmp, pre, data=df, color=factor(prs), alpha=I(0.7), xlab = vargrp.t[i], ylab = vargrp.p[i], main = i)
-  p <- p + scale_colour_manual(values = cols)
-  p <- p + xlim(min(df$tmp), max(df$tmp)) + ylim(min(df$pre), max(df$pre))
-  p <- p + theme(title =element_text(size=14, face='bold'), axis.text=element_text(size=10),axis.title=element_text(size=12,face="bold"),legend.position="none")
-  return(p) 
+  df.t <- read.csv(paste0(outcsvpath, vargrp.t[i], "_",years[1], "_",years[nyr], ".csv"))
+  df.p <- read.csv(paste0(outcsvpath, vargrp.p[i], "_",years[1], "_",years[nyr], ".csv"))
+  df <- cbind(data.frame(tmp=df.t[,1]),data.frame(pre=df.p[,1]),data.frame(prs=df.t[,2]))
+  df.na <- subset(df, prs=="continent")
+  plot(df.na$tmp, df.na$pre, pch=16, cex=0.1, col = alpha(cols[1], 0.2), main=paste0(vargrp.t[i],"/",vargrp.p[i]), xlab=vargrp.t[i], ylab=vargrp.p[i], cex.main=2, cex.lab=1.5, cex.axis=1.5)
+  df.vgt <- subset(df, prs=="hosts")
+  points(df.vgt$tmp, df.vgt$pre, pch=16, cex=0.1, col = alpha(cols[2], 0.2))
+  df.btl <- subset(df, prs=="mpb")
+  points(df.btl$tmp, df.btl$pre, pch=16, cex=0.1, col = alpha(cols[3], 0.2))  
 }
 png("cs_monthly_var_union.png", width=16, height=10, units="in", res=300)
-grid.newpage()
-par(mar=c(2,2,4,2))
-pushViewport(viewport(layout = grid.layout(3, 5)))
+par(mfrow=c(3,5),mar=c(5,5,3,1))
 for(i in 1:length(vargrp.t)){
-  print(climate.space(i), vp = vplayout(n1[i], n2[i]))
+  climate.space(i)
   print(paste0("plotting climate space with ", vargrp.t[i], " and ", vargrp.p[i]))
 }
+plot(0,type='n',axes=FALSE,ann=FALSE)
+legend('center', pch=16, col=cols, legend=c("Continent", "Hosts", "Beetles"), cex = 2, bty='n')
 dev.off()
 
 print("all done!")		  
