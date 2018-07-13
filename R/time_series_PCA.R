8def5ffa546bee3665fb34c7c388ac5b12b2bad8
dongmeic <dongmeijane1107@gmail.com>
2018-07-12 23:20:21 -0700

    tsPCA

--- R/time_series_PCA.R
+++ R/time_series_PCA.R
@@ -1,5 +1,11 @@
 # Created by Damian Satterthwaite-Phillips
 # Modified by Dongmei Chen, from PCA_bioclimatic_variables.R
+# issues with years 2001, 2007, 2012, 2013, 2015 on a bash mode
+
+library(parallel)
+library(doParallel)
+library(foreach)
+registerDoParallel(cores=28)
 
 csvpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/"
 out <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/plots/"
@@ -9,34 +15,46 @@ na10km_btl_df <- read.csv("/gpfs/projects/gavingrp/dongmeic/beetle/output/tables
 head(na10km_btl_df)
 years <- 1996:2015
 
-pca_plot <- function(df,i,pca){
-	png(paste0("PCA_variable_selection_",years[i],".png"), width=12, height=9, units="in", res=300)
+ndf <- read.csv(paste0(csvpath,"bioclimatic_values_",years[1],".csv")) # from bioclimatic_values_time_series.R
+ndf <- cbind(ndf, na10km_btl_df[,c(paste0("prs_",(years[1]+1)),"vegetation")])
+colnames(ndf)[23:24] <- c("beetles","hosts")
+for(i in 2:length(years)){
+  df <- read.csv(paste0(csvpath,"bioclimatic_values_",years[i],".csv"))
+  df <- cbind(df,na10km_btl_df[,c(paste0("prs_",(years[i]+1)),"vegetation")])
+  colnames(df)[23:24] <- c("beetles","hosts")
+  ndf <- rbind(ndf,df)
+  print(paste(years[i], "done!"))
+}
+ndf <- cbind(ndf, year=unlist(lapply(1996:2015,function(i) rep(i,dim(ndf)[1]/length(1996:2015)))))
+
+pca_plot <- function(df,j){
+	png(paste0("PCA_variable_selection_",years[j],".png"), width=12, height=9, units="in", res=300)
 	par(mfrow=c(2, 2))
 	par(mar=c(4, 4, 3, 3))
 	t = 0.01
 	for(i in c(1, 3)) {
-	  plot(pca$scores[df$hosts == 0, i], 
-		   pca$scores[df$hosts == 0, i + 1], 
+	  plot(pca$scores[df$hosts == 0 & df$year == years[j], i], 
+		   pca$scores[df$hosts == 0 & df$year == years[j], i + 1], 
 		   col=rgb(1, 0, 0, t), 
 		   pch=16,
 		   xlab=paste('PC', i, sep=''),
 		   ylab=paste('PC', i + 1, sep=''), 
 		   main='Host Tree')
-	  points(pca$scores[df$hosts == 1, i],
-			 pca$scores[df$hosts == 1, i + 1],
+	  points(pca$scores[df$hosts == 1 & df$year == years[j], i],
+			 pca$scores[df$hosts == 1 & df$year == years[j], i + 1],
 			 pch=16,
 			 col=rgb(0, 1, 0, t))
 	  legend('bottomleft', pch=16, col=c(2, 3), legend=c('absent', 'present'))
-	  plot(pca$scores[df$beetles == 0, i], 
-		   pca$scores[df$beetles == 0, i + 1], 
+	  plot(pca$scores[df$beetles == 0 & df$year == years[j], i], 
+		   pca$scores[df$beetles == 0 & df$year == years[j], i + 1], 
 		   col=rgb(1, 0, 1, t), 
 		   pch=16,
 		   xlab=paste('PC', i, sep=''),
 		   ylab='', 
 		   yaxt='n',
 		   main='Beetle')
-	  points(pca$scores[df$beetles == 1, i],
-			 pca$scores[df$beetles == 1, i + 1],
+	  points(pca$scores[df$beetles == 1 & df$year == years[j], i],
+			 pca$scores[df$beetles == 1 & df$year == years[j], i + 1],
 			 pch=16,
 			 col=rgb(0, 1, 1, t))
 	  legend('bottomleft', pch=16, col=c(6, 5), legend=c('absent', 'present'))
@@ -45,18 +63,11 @@ pca_plot <- function(df,i,pca){
 	dev.off()
 }
 
-for(i in 1:length(years)){
-  df <- read.csv(paste0(csvpath,"bioclimatic_values_",years[i],".csv"))
-  df <- cbind(df,na10km_btl_df[,c(paste0("prs_",(years[i]+1)),"vegetation")])
-  colnames(df)[23:24] <- c("beetles","hosts")
-  vars <- 1:22
-  pca <- princomp(df[, vars], cor=T)
-  summary(pca, loadings <- T)
-	sink(paste0(csvpath,"pca_summary_",years[i],".txt"))
-	summary(pca)
-	sink()
-	write.csv(x = unclass(loadings(pca)), file = paste0(csvpath,"pca_loading_",years[i],".csv"))
-  pca_plot(df,i,pca)
+vars <- 1:22
+pca <- princomp(ndf[, vars], cor=T)
+
+foreach(i=1:length(years))%dopar%{
+	pca_plot(ndf, i)
 	print(paste(years[i], "done!"))
 }
 
