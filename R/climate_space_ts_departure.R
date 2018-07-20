06b43862d3fd00d4e4629052c2a35bb3f19736fa
dongmeic <dongmeijane1107@gmail.com>
2018-07-20 12:19:18 -0700

    ts_std

--- R/climate_space_ts_departure.R
+++ R/climate_space_ts_departure.R
@@ -14,21 +14,21 @@ vargrp1 <- c("fallTmean", "winterTmin", "Tmin", "Tmean", "Tvar", "JanTmin", "Pcu
 vargrp2 <- c("TOctSep", "TMarAug", "summerTmean", "AugTmean", "AugTmax", "MarTmin", "PPT", "Pmean", "POctSep", "PMarAug")
 cols <- c("grey70", "#1b9e77", "#d95f02")
 
-n1 <- rep(c(1,2,3,4),5); n2 <- c(rep(1,4),rep(2,4),rep(3,4),rep(4,4),rep(5,4))
+l1 <- rep(c(1,2,3,4),5); l2 <- c(rep(1,4),rep(2,4),rep(3,4),rep(4,4),rep(5,4))
 for (i in 1:length(vargrp1)){
   df.t <- read.csv(paste0(csvpath, vargrp1[i], "_std_", years[1], "_", years[nyr], ".csv"))
   df.p <- read.csv(paste0(csvpath, vargrp2[i], "_std_", years[1], "_", years[nyr], ".csv"))
   df <- cbind(data.frame(v1=df.t[,1]),data.frame(v2=df.p[,1]),data.frame(prs=df.t[,2]),data.frame(yrs=df.t[,3]))
   climate.space <- function(j){
-    df.ss <- subset(df, yrs==years[j])
+    df.ss <- subset(df, yrs==years[j] & prs=="mpb")
     sd1 <- length(na.omit(df.ss[(df.ss$v1 <= 1 & df.ss$v1 > 0) | (df.ss$v1 >= -1 & df.ss$v1 < 0),]$v1))
     sd2 <- length(na.omit(df.ss[(df.ss$v1 <= 2 & df.ss$v1 > 1) | (df.ss$v1 < -1 & df.ss$v1 >= -2),]$v1))
     sd3 <- length(na.omit(df.ss[(df.ss$v1 <= 3 & df.ss$v1 > 2) | (df.ss$v1 < -2 & df.ss$v1 >= -3),]$v1))
     sd4 <- length(na.omit(df.ss[(df.ss$v2 <= 1 & df.ss$v2 > 0) | (df.ss$v2 >= -1 & df.ss$v2 < 0),]$v2))
     sd5 <- length(na.omit(df.ss[(df.ss$v2 <= 2 & df.ss$v2 > 1) | (df.ss$v2 < -1 & df.ss$v2 >= -2),]$v2))
     sd6 <- length(na.omit(df.ss[(df.ss$v2 <= 3 & df.ss$v2 > 2) | (df.ss$v2 < -2 & df.ss$v2 >= -3),]$v2))
-    sdsum1 <- length(na.omit(df.ss[df.ss$prs=="mpb",]$v2))
-    sdsum <- length(na.omit(df.ss[df.ss$prs=="mpb",]$v1))
+    sdsum1 <- length(na.omit(df.ss$v2))
+    sdsum <- length(na.omit(df.ss$v1))
     n1 <- round(sd1/sdsum, digits = 1)
     n2 <- round(sd2/sdsum, digits = 1)
     n3 <- round(sd3/sdsum, digits = 1)
@@ -48,7 +48,7 @@ for (i in 1:length(vargrp1)){
   par(mar=c(2,2,4,2))
   pushViewport(viewport(layout = grid.layout(4, 5)))
   for(yr in 1:20){
-  	print(climate.space(yr), vp = vplayout(n1[yr], n2[yr]))
+  	print(climate.space(yr), vp = vplayout(l1[yr], l2[yr]))
   }
   dev.off()
   print(paste("The time-series climate space of departure with variables", vargrp1[i], "and", vargrp2[i], "is done!"))  
