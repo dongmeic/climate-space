9c301aa749251db5fadfbf8f5b6167a1ec0dff3d
dongmeic <dongmeijane1107@gmail.com>
2018-07-26 13:43:31 -0700

    adjusted climate space density plots and boxplots

--- R/climate_space_plots.R
+++ R/climate_space_plots.R
@@ -6,20 +6,26 @@ ndf <- read.csv(paste0(csvpath,csvfile))
 out <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/plots"
 setwd(out)
 
-vargrp <- c("OctTmin", "fallTmean", "winterTmin", "JanTmin", "MarTmin", "Tmin", "Tmean", 
-			"Tvar", "TOctSep", "TMarAug", "summerTmean", "AugTmean", "AugTmax", 
-			 "PcumOctSep", "summerP0", "summerP1", "summerP2", "PPT", "Pmean", 
-			 "GSP", "POctSep", "PMarAug")
+vargrp.t <- c("Tmin", "MarTmin", "TOctSep", "Tmean", "fallTmean", "OctTmin", "winterTmin",
+							"JanTmin", "ddAugJun", "ddAugJul", "TMarAug", "summerTmean")
+							
+vargrp.p <- c("AugTmean", "AugTmax", "Tvar", "PMarAug", "PcumOctSep", "PPT", "Pmean",
+              "POctSep", "summerP2", "GSP", "summerP0", "summerP1")
+
+vargrp <- c(vargrp.t, vargrp.p)
+
+cols <- c("grey70", "#1b9e77", "#7570b3")
 
 # density plot
 density.plot <- function(i){
   p1 <- density(ndf[,vargrp[i]])
   p2 <- density(ndf[ndf$hosts==1,][,vargrp[i]])
   p3 <- density(ndf[ndf$beetles==1,][,vargrp[i]])
-  plot(p3,col='red', main=vargrp[i], xlab="", ylab="", cex.main=2, cex.lab=1.5, cex.axis=1.5, lwd=4)
-  lines(p2,col='green', lwd=4)
-  lines(p1,col='grey', lwd=4)
-  polygon(p3, col=rgb(1,0,0,0.2), border='red')
+  r <- range(c(p1$y,p2$y,p3$y))
+  plot(p3,col=cols[1], main=vargrp[i], xlab="", ylab="", cex.main=2, cex.lab=1.5, cex.axis=1.5, lwd=4, ylim=r)
+  lines(p2,col=cols[2], lwd=4)
+  lines(p1,col=cols[3], lwd=4)
+  polygon(p3, col=rgb(1,0,0,0.2), border=cols[3])
   print(paste(vargrp[i], "is done!"))
 }
 
@@ -27,14 +33,15 @@ for(i in 1:length(vargrp)){
   p1 <- density(ndf[,vargrp[i]])
   p2 <- density(ndf[ndf$hosts==1,][,vargrp[i]])
   p3 <- density(ndf[ndf$beetles==1,][,vargrp[i]])
+  r <- range(c(p1$y,p2$y,p3$y))
   png(paste0("bioclimatic_density_",vargrp[i],".png"), width=9, height=8, units="in", res=300)
   par(mfrow=c(1, 1), mar=c(4.5,4.5,3,1))
-  plot(p3,col='red', main=vargrp[i], xlab="Values", cex.main=2, cex.lab=1.5, cex.axis=1.5, lwd=4)
-  lines(p2,col='green', lwd=4)
-  lines(p1,col='grey', lwd=4)
-  polygon(p3, col=rgb(1,0,0,0.2), border='red') 
+  plot(p3,col=cols[1], main=vargrp[i], xlab="Values", cex.main=2, cex.lab=1.5, cex.axis=1.5, lwd=4, ylim=r)
+  lines(p2,col=cols[2], lwd=4)
+  lines(p1,col=cols[3], lwd=4)
+  polygon(p3, col=rgb(1,0,0,0.2), border=cols[3]) 
   rug(ndf[ndf$beetles==1,][,vargrp[i]],col=rgb(1,0,0,0.05))
-  legend('topright', lty=1, lwd=4, col=c("grey", "green", "red"), legend=c("Continent", "Hosts", "Beetles"), cex = 1.8, bty='n')
+  legend('topright', lty=1, lwd=4, col=cols, legend=c("Continent", "Hosts", "Beetles"), cex = 1.8, bty='n')
   dev.off()
   print(paste(vargrp[i], "is done!"))
 }
@@ -43,9 +50,12 @@ png("bioclimatic_density_plots.png", width=18, height=12, units="in", res=300)
 par(mfrow=c(4,6),mar=c(3.5,3.5,3,1))
 for (i in 1:length(vargrp)){
   density.plot(i)
+  if(i==1){
+    legend('center', lty=1, lwd=4, col=cols, legend=c("Continent", "Hosts", "Beetles"), cex = 2, bty='n')
+  }
 }
-plot(0,type='n',axes=FALSE,ann=FALSE)
-legend('center', lty=1, lwd=4, col=c("grey", "green", "red"), legend=c("Continent", "Hosts", "Beetles"), cex = 2, bty='n')
+#plot(0,type='n',axes=FALSE,ann=FALSE)
+#legend('center', lty=1, lwd=4, col=cols, legend=c("Continent", "Hosts", "Beetles"), cex = 2, bty='n')
 dev.off()
 
 # boxplot
@@ -57,9 +67,9 @@ presence.boxplot <- function(i){
   beetles.df <- ndf[,c(vargrp[i],"beetles")]
   colnames(beetles.df)[1] <- "bioclm"
   beetles.df$beetles <- as.character(beetles.df$beetles)
-  boxplot(bioclm~hosts,data=hosts.df, main=vargrp[i], horizontal = TRUE,col=rgb(0,1,0,0.5), outcol=rgb(0,0,0,0.1),outcex=0.5,
+  boxplot(bioclm~hosts,data=hosts.df, main=vargrp[i], horizontal = TRUE,col="#1B9E777D", outcol=rgb(0,0,0,0.1),outcex=0.5,
    xlab="", ylab="") 
-  boxplot(bioclm~beetles,data=beetles.df, main=vargrp[i], horizontal = TRUE,col=rgb(1,0,0,0.5),outcol=rgb(0,0,0,0.1),outcex=0.5,
+  boxplot(bioclm~beetles,data=beetles.df, main=vargrp[i], horizontal = TRUE,col="#7570B37D",outcol=rgb(0,0,0,0.1),outcex=0.5,
    xlab="", ylab="")
    print(paste(vargrp[i], "is done!"))
 }
@@ -73,9 +83,9 @@ for(i in 1:length(vargrp)){
   beetles.df$beetles <- as.character(beetles.df$beetles)
   png(paste0("presence_boxplot_",vargrp[i],".png"), width=8, height=6, units="in", res=300)
   par(mfrow=c(2,1),mar=c(4.5,4.5,3,1))
-  boxplot(bioclm~hosts,data=hosts.df, main="Hosts", horizontal = TRUE,col=rgb(0,1,0,0.5), outcol=rgb(0,0,0,0.1),outcex=0.5,
+  boxplot(bioclm~hosts,data=hosts.df, main="Hosts", horizontal = TRUE,col="#1B9E777D", outcol=rgb(0,0,0,0.1),outcex=0.5,
    xlab="", ylab="Presence") 
-  boxplot(bioclm~beetles,data=beetles.df, main="Beetles", horizontal = TRUE,col=rgb(1,0,0,0.5),outcol=rgb(0,0,0,0.1),outcex=0.5,
+  boxplot(bioclm~beetles,data=beetles.df, main="Beetles", horizontal = TRUE,col="#7570B37D",outcol=rgb(0,0,0,0.1),outcex=0.5,
    xlab=vargrp[i], ylab="Presence")   
   dev.off() 
   print(paste(vargrp[i], "is done!"))
@@ -88,9 +98,9 @@ par(mar=c(3,3,3,1))
 for (i in 1:length(vargrp)){
   presence.boxplot(i)
 }
-plot(0,type='n',axes=FALSE,ann=FALSE)
-legend('bottomleft',legend="Hosts",fill=rgb(0,1,0,0.5), cex = 2, bty='n')
-plot(0,type='n',axes=FALSE,ann=FALSE)
-legend('topleft',legend= "Beetles",fill=rgb(1,0,0,0.5), cex = 2, bty='n')
-dev.off()
+# plot(0,type='n',axes=FALSE,ann=FALSE)
+# legend('bottomleft',legend="Hosts",fill="#1B9E777D", cex = 2, bty='n')
+# plot(0,type='n',axes=FALSE,ann=FALSE)
+# legend('topleft',legend= "Beetles",fill="#7570B37D", cex = 2, bty='n')
+# dev.off()
 print("all done!")
