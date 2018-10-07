e062f317a26222f4c7805703e793ff9373c485da
dongmeic <dongmeijane1107@gmail.com>
2018-10-06 22:57:00 -0700

    added more daily variables in LDA and correlation matrix

--- R/linear_discriminant_analysis.R
+++ R/linear_discriminant_analysis.R
@@ -1,13 +1,27 @@
 # Created by Dongmei Chen
+# run in an interactive mode
 
 library(MASS)
+library(Hmisc)
+install.packages("corrplot", repos='http://cran.us.r-project.org')
+library(corrplot)
+install.packages("PerformanceAnalytics", repos='http://cran.us.r-project.org')
+library(PerformanceAnalytics)
+
 inpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/"
 data <- read.csv(paste0(inpath, "bioclimatic_values_1996_2015_r.csv"))
-dat <- data[,-35]
-dat <- cbind(dat[34], scale(dat[,-34]))
+d <- dim(data)[2]
+# Remove hosts column
+dat <- data[,-d]
+d <- dim(dat)[2]
+# rescale the predictors
+dat <- cbind(dat[d], scale(dat[,-d]))
 dat$beetles <- as.character(dat$beetles)
-# Linear Discriminant Analysis with Jacknifed Prediction 
+# Linear Discriminant Analysis
 dat.lda <- lda(beetles ~ ., data=dat)
+sink(paste0(inpath,"lda.txt"))
+print(dat.lda)
+sink()
 
 # Assess the accuracy of the prediction
 # percent correct for each category of "beetles"
@@ -17,5 +31,33 @@ diag(prop.table(ct, 1))
 # total percent correct
 sum(diag(prop.table(ct)))
 
-# Scatter plot using the 1st two discriminant dimensions
-plot(fit) # fit from lda
+# correlation matrix
+my_data <- scale(dat[,-d])
+res <- cor(my_data)
+round(res, 2)
+res2 <- rcorr(my_data)
+
+# source: http://www.sthda.com/english/wiki/correlation-matrix-a-quick-start-guide-to-analyze-format-and-visualize-a-correlation-matrix-using-r-software
+# ++++++++++++++++++++++++++++
+# flattenCorrMatrix
+# ++++++++++++++++++++++++++++
+# cormat : matrix of the correlation coefficients
+# pmat : matrix of the correlation p-values
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+    )
+}
+sink(paste0(inpath,"flattenCorrMatrix.txt"))
+flattenCorrMatrix(res2$r, res2$P)
+sink()
+
+corrplot(res, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
+
+chart.Correlation(my_data, histogram=TRUE, pch=19)
+
