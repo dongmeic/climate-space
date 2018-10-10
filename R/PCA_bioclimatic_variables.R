23643827649e98eec0e4af6ec649633c019ba9c4
dongmeic <dongmeijane1107@gmail.com>
2018-10-09 21:38:17 -0700

    corrected the raw data output for LDA and further analysis

--- R/PCA_bioclimatic_variables.R
+++ R/PCA_bioclimatic_variables.R
@@ -1,27 +1,9 @@
 # Created by Damian Satterthwaite-Phillips
 # Modified by Dongmei Chen
 
-#inpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/climate_space/paired/"
 csvpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/"
-#infile <- "bioclimatic_variables_1996_2015.csv"
 out <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/plots/"
 setwd(out)
-#indata <- read.csv(paste0(inpath,infile))
-#dim(indata);head(indata)
-
-#df <- subset(indata, prs=="continent")
-#vars <- 1:22
-#pca <- princomp(df[, vars], cor=T)
-#summary(pca, loadings <- T)
-
-#sink("pca_summary.txt")
-#summary(pca)
-#sink()
-#file.show("pca_summary.txt")
-
-#write.csv(x = unclass(loadings(pca)), file = "pca_loading.csv")
-#write.csv(x = unclass(loadings(pca))[,(pca$sdev^2 > 1),drop = FALSE], file = "pca_loading.csv")
-#str(pca)
 
 na10km_btl_df <- read.csv("/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/beetle_presence.csv")
 head(na10km_btl_df)
@@ -40,7 +22,7 @@ for(i in 2:length(years)){
   print(paste(years[i], "done!"))
 }
 ndf <- cbind(ndf, year=unlist(lapply(1996:2015,function(i) rep(i,dim(ndf)[1]/length(1996:2015)))))
-write.csv(df, paste0(csvpath, "bioclimatic_values_1996_2015_r.csv"), row.names=FALSE)
+write.csv(ndf, paste0(csvpath, "bioclimatic_values_1996_2015_r.csv"), row.names=FALSE)
 svars <- c("GSP", "PMarAug", "summerP0","summerP1", "summerP2", 
            "Pmean","POctSep", "PcumOctSep", "PPT", "ddAugJul", "ddAugJun")
 sdf <- sqrt(ndf[, svars])
