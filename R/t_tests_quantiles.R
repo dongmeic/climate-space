5f97212af06776cf7e18340576f2df89633b2795
dongmeic <dongmeijane1107@gmail.com>
2018-10-20 21:18:39 -0700

    simulate a tests on quantiles between peak and non-peak years

--- R/t_tests_quantiles.R
+++ R/t_tests_quantiles.R
@@ -4,38 +4,62 @@ inpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/"
 setwd(inpath)
 
 # use transformed data
-data <- read.csv(paste0(inpath, "bioclimatic_values_1996_2015_t.csv"))
+data <- read.csv(paste0(inpath, "bioclimatic_values_1996_2015_r.csv"))
 head(data)
 dt <- data[data$beetles==1,]
+na10km <- read.csv("/gpfs/projects/gavingrp/dongmeic/beetle/csvfiles/na10km_v2.csv")
+#write.csv(dt, paste0(inpath, "bioclimatic_values_presence.csv"), row.names=FALSE)
+#dt <- read.csv(paste0(inpath, "bioclimatic_values_presence.csv"))
 
 peakyears <- 2006:2010
 dt$peak <- ifelse(dt$year %in% peakyears, 1, 0)
 
-x <- sample(dt[dt$peak==1,][,'ddAugJul'],5000)
-y <- sample(dt[dt$peak==0,][,'ddAugJul'],5000)
+vars <- c("ddAugJul","AugTmax","winterTmin","summerP0","PPT","GSP","summerP1","Tvar")
 
-d <- vector()
-times <- seq(500,1000,by=10)
-for(i in 1:length(times)){
-	d[i] <- mean(replicate(times[i], sample(dt$ddAugJul, 5000)))
-	print(i)
+if(0){
+	d <- vector()
+	times <- seq(500,1000,by=10)
+	for(i in 1:length(times)){
+		d[i] <- mean(replicate(times[i], sample(dt$ddAugJul, 5000)))
+		print(i)
+	}
 }
 
 tau <- c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95)
-peak <- data.frame(q1=double(), q2=double(), q3=double(), q4=double(), q5=double(), q6=double(), q7=double())
-nonpeak <- data.frame(q1=double(), q2=double(), q3=double(), q4=double(), q5=double(), q6=double(), q7=double())
+for(i in 1:length(vars)){
+	m1 <- replicate(1000, quantile(sample(dt[dt$peak==1,][,vars[i]],5000), tau))
+	write.csv(m1, paste0(inpath, "quantile/peak_", vars[i], "_r.csv"), row.names=FALSE)
+	m2 <- replicate(1000, quantile(sample(dt[dt$peak==0,][,vars[i]],5000), tau))
+	write.csv(m2, paste0(inpath, "quantile/nonpeak_", vars[i], "_r.csv"), row.names=FALSE)
+	print(i)
+}
 
-m <- replicate(1000, quantile(sample(dt[dt$peak==1,][,'ddAugJul'],5000), tau))
-
-ts = replicate(1000,t.test(sample(dt[dt$peak==1,][,'ddAugJul'],5000),sample(dt[dt$peak==0,][,'ddAugJul'],5000))$statistic)
-tps = replicate(1000,t.test(sample(dt[dt$peak==1,][,'ddAugJul'],5000),sample(dt[dt$peak==0,][,'ddAugJul'],5000))$p.value)
-
-par(mfrow=c(1, 2))
-boxplot(sample(dt[dt$peak==1,][,'ddAugJul'],5000))
-boxplot(sample(dt[dt$peak==0,][,'ddAugJul'],5000))
-
-hist(ts, prob=TRUE)
-lines(density(ts), col='red')
-
-hist(tps, prob=TRUE)
-lines(density(tps), col='red')
+t.df <- as.data.frame(matrix(,ncol=0,nrow=7))
+p.df <- as.data.frame(matrix(,ncol=0,nrow=7))
+e.df <- as.data.frame(matrix(,ncol=0,nrow=14))
+for(i in 1:length(vars)){
+	m1 <- read.csv(paste0(inpath, "quantile/peak_", vars[i], "_r.csv"))
+	m2 <- read.csv(paste0(inpath, "quantile/nonpeak_", vars[i], "_r.csv"))
+	t <- vector(); p <- vector(); e1 <- vector(); e2 <- vector()
+	for(j in 1:length(tau)){
+		s1 <- sample(as.numeric(m1[j,]),100); s2 <- sample(as.numeric(m2[j,]),100)
+		p1 <- as.numeric(m1[j,]); p2 <- as.numeric(m2[j,])
+		t[j] <- median(replicate(10,t.test(s1, s2)$statistic))
+		p[j] <- median(replicate(10,t.test(s1, s2)$p.value))
+		e1[j] <- as.numeric(t.test(p1, p2)$estimate[1])
+		e2[j] <- as.numeric(t.test(p1, p2)$estimate[2])
+	}
+	t.df <- cbind(t.df, t)
+	p.df <- cbind(p.df, p)
+	e3 <- e1 - e2
+	e.df <- cbind(e.df, e1, e2, e3)
+	names(t.df)[dim(t.df)[2]] <- vars[i]
+	names(p.df)[dim(p.df)[2]] <- vars[i]
+	names(e.df)[dim(e.df)[2]-2] <- paste0(vars[i], "_x")
+	names(e.df)[dim(e.df)[2]-1] <- paste0(vars[i], "_y")
+	names(e.df)[dim(e.df)[2]] <- paste0(vars[i], "_z")
+	print(vars[i])
+}
+write.csv(t.df, paste0(inpath, "quantile/tvalues_median.csv"), row.names=FALSE)
+write.csv(p.df, paste0(inpath, "quantile/pvalues_median.csv"), row.names=FALSE)
+write.csv(e.df, paste0(inpath, "quantile/estimates.csv"), row.names=FALSE)
