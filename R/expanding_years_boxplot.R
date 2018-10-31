d7c948eb03d125c8cf4c256488ce1ee7bc93c4e6
dongmeic <dongmeijane1107@gmail.com>
2018-10-31 11:00:29 -0700

    added boxplots of bioclimate during the expanding years

---
+++ R/expanding_years_boxplot.R
@@ -0,0 +1,36 @@
+# run in an interactive mode
+
+inpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/"
+setwd(inpath)
+
+dt <- read.csv(paste0(inpath, "bioclimatic_values_presence.csv"))
+
+peakyears <- 2006:2008
+nonpeakyears <- 1996:1998
+expanding1 <- 2000:2002
+expanding2 <- 2003:2005
+dt$peak <- ifelse(dt$year %in% peakyears, 4, 
+					 ifelse(dt$year %in% nonpeakyears, 1, 
+					 ifelse(dt$year %in% expanding1,2,
+					 ifelse(dt$year %in% expanding2,3,0))))
+
+vars <- c("ddAugJul","AugTmax","winterTmin","summerP0","PPT","GSP","summerP1","Tvar")
+
+peak.boxplot <- function(var){
+  df <- dt[dt$peak!=0,][,c(var,"peak")]
+  colnames(df)[1] <- "bioclm"
+  boxplot(bioclm~peak,data=df, main=var, cex=1.5, horizontal = TRUE,col=cols, outcol=rgb(0,0,0,0.5),outcex=0.5,
+   xlab="", ylab="")
+}
+
+cols <- rev(c('#e41a1c','#377eb8','#4daf4a','#984ea3'))
+out <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/plots/"
+png(paste0(out,"expanding_years_boxplot.png"), width=12, height=6, units="in", res=300)
+par(mfrow=c(2,4),mar=c(3.5,3.5,3,1))
+for (var in vars){
+  peak.boxplot(var)
+  if(var=="ddAugJul"){
+    legend('right', fill=cols, ncol=2, legend=c("1:1996-1998","2:2000-2002","3:2003-2005","4:2006-2008"), cex = 0.8, bty='n')
+  }
+}
+dev.off()
