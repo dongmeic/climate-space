b4b48fd749506b174b7ad45b0e587922b5ce7ff3
dchen <dongmeic@uoregon.edu>
2019-08-24 23:46:38 -0700

    get data

--- R/calc_AUC.R
+++ R/calc_AUC.R
@@ -1,3 +1,7 @@
 library(pROC)
 
 source("/Users/dongmeichen/GitHub/climate-space/R/combine_CRU_Daymet.R")
+ndf <- get_data()
+ndf <- ndf[complete.cases(ndf),]
+ndf$hosts <- ifelse(ndf$beetles==1 & ndf$hosts==0, 1, ndf$hosts)
+out <- "/Users/dongmeichen/Documents/beetle/output/"
