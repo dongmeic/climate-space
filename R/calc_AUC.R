043cb08f1cb0bc12660d56c32d7447146ac1a6ec
dchen <dongmeic@uoregon.edu>
2019-08-25 03:49:36 -0700

    univariate plots

--- R/calc_AUC.R
+++ R/calc_AUC.R
@@ -1,4 +1,5 @@
 library(pROC)
+library(openxlsx)
 
 source("/Users/dongmeichen/GitHub/climate-space/R/combine_CRU_Daymet.R")
 ndf <- get_data()
@@ -16,11 +17,43 @@ vargrp <- c("OctTmin", "JanTmin", "MarTmin", "Tmin", "OctMin", "JanMin", "MarMin
 n <- dim(ndf)[1]
 
 ptm <- proc.time()
-sink(paste0(out,"auc.txt"))
+sink("auc.txt")
 for (i in 1:length(vargrp)){
   print(auc(ndf[, "beetles"], ndf[,vargrp[i]]))
-  print(i)
+  #print(i)
 }  
 sink()
 proc.time() - ptm
+
+auc <- read.xlsx("/Users/dongmeichen/Documents/beetle/data/auc.xlsx")
+auc$VAR <- vargrp
+write.csv(paste0(out, "auc.csv"), row.names=FALSE)
+
+univariate.plot <- function(i){
+  df <- ndf[,c(vargrp[i], "hosts", "beetles")]
+  df <- df[sample(nrow(df), 0.05*n),]
+  df1 <- df[df$hosts==1,]
+  df2 <- df[df$beetles==1,]
+  df3 <- rbind(df, df1, df2)
+  df3$ucs <- c(rep("Continent", dim(df)[1]), rep("Hosts", dim(df1)[1]), rep("MPB", dim(df2)[1]))
+  names(df3)[which(names(df3)==vargrp[i])] <- "var"
+  stripchart(var~ucs,
+             data=df3,
+             main=paste0(vargrp[i], ": ", round(auc$AUC[auc$VAR==vargrp[i]], 3)),
+             xlab="",
+             ylab="",
+             cex=0.5,
+             cex.main=2, cex.lab=1.5, cex.axis=1.5,
+             #col=rgb(0.5, 0.5, 0.5, 0.5),
+             pch=19)
+  print(paste(vargrp[i], "is done!"))
+}
+
+png("bioclim_univariate_plots_auc.png", width=15, height=24, units="in", res=300)
+par(mfrow=c(8,5),mar=c(3.5,3.5,3,1))
+for (i in 1:length(vargrp)){
+  univariate.plot(i)
+}
+dev.off()
+
 
